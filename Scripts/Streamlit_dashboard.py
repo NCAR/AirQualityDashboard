382dffc2a1174a4642d2279bb2f1378b5082267b
mcasali <50184471+mcasali@users.noreply.github.com>
2022-03-08 15:23:19 -0700

    Adding scripts

---
+++ Scripts/Streamlit_dashboard.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import zarr
+import numpy as np
+
+
+@st.cache
+def get_data():
+    return zarr.load('.data/Zarr/array_XS.zarr')
+
+
+z = get_data()
+
+st.header("Air Quality Index User Dashboard")
+st.write("""
+Description of how to use page \n
+Description of what variables are
+""")
+
