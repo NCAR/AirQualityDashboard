ee88eb891cea063d2aa0e7d759e72a4caa1f9741
mcasali <50184471+mcasali@users.noreply.github.com>
2022-03-09 10:53:38 -0700

    Testing parquet file

--- Scripts/Streamlit_app.py
+++ Scripts/Streamlit_app.py
@@ -6,16 +6,16 @@ import numpy as np
 
 @st.cache
 def get_data():
-    return zarr.load('https://github.com/mcasali/AirQualityDashboard/blob/main/Data/Zarr/testZarr.zip')
+    return pd.read_parquet('https://github.com/mcasali/AirQualityDashboard/blob/main/Data/Parquet/testPar.parquet')
 
 
-z = get_data()
+df = get_data()
 
 st.header("Air Quality Index User Dashboard")
 st.write("""
 Description of how to use page \n
 Description of what variables are
 """)
-test = str(z[0][0])
+test = str(df["A"][0])
 st.write(test)
 
