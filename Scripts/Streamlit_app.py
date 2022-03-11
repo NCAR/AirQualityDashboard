5c749fd35c2fc870f4cde43174c1ba8394c03bd6
mcasali <50184471+mcasali@users.noreply.github.com>
2022-03-11 14:51:16 -0700

    added text

--- Scripts/Streamlit_app.py
+++ Scripts/Streamlit_app.py
@@ -4,19 +4,29 @@ import zarr
 import numpy as np
 
 
-# @st.cache
+@st.cache
 def get_data():
     return pd.read_parquet('https://github.com/mcasali/AirQualityDashboard/blob/main/Data/Testing/test.parquet',
                            engine="fastparquet")
 
 
-df = get_data()
+# df = get_data()
 
 st.header("Air Quality Index User Dashboard")
 st.write("""
 Description of how to use page \n
-Description of what variables are
+AQI... \n
+Cloud fraction...\n
+CO...\n
+MDA8...\n
+NO2...\n
+pm10...\n
+pm1... \n
+pm25... \n
+SO2... \n
+SWDOWN ... \n
+T2 max ...\n
+T2 min ... \n
 """)
-test = str(df["station_id"][0])
-st.write(test)
 
+st.map()
